ca515ab80a1248becc4b2978a970d26600ef9a49
Mihai Miron <mihaimiron004@gmail.com>
2025-02-19 22:33:31 +0200

    Add controler for exchange and transfer pages

---
+++ Lab_1/Controllers/exchange_rateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class exchange_rateController : Controller
+    {
+        // GET: exchange_rate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab_1/Controllers/transferController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class transferController : Controller
+    {
+        // GET: transfer
+        public ActionResult a2a()
+        {
+            return View();
+        }
+
+        public ActionResult p2p()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult afm()
+        {
+            return View();
+        }
+
+        public ActionResult supply()
+        {
+            return View();
+        }
+
+
+
+    }
+}
