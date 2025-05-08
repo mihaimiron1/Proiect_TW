2a4169c9e6ef54e859d107fde0e6c087352e2e88
alexiamihniova <alexia.mihniova@isa.utm.md>
2025-05-08 13:02:32 +0300

    add card_page

---
+++ eUseControl.Web/Controllers/CardController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class CardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+} 
